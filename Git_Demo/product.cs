0a6239d20e6d9ed7c4ebc04dbf60f86c281aa19e
Engr. Hamid Idrees <hamididrees457@gmail.com>
2025-06-27 17:04:35 +0500

    add product class

---
+++ Git_Demo/product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git_Demo
+{
+    public class product
+    {
+        public int id;
+        public string name;
+        public string description;
+        public decimal price;
+        public int quantity;
+
+    }
+}
