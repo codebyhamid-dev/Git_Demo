325021085b39f63bbaf81570ee8e17e6facafb1b
Engr. Hamid Idrees <hamididrees457@gmail.com>
2025-06-27 17:17:46 +0500

    create data access layer

---
+++ Git_Demo/Data/DAL.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git_Demo.Data
+{
+    public class DAL
+    {
+        public string Damage { get; set; }
+    }
+}
